f606a1c037e05b12a38077a2a104b75e1f016a14
SJcodeML <144626657+SJcodeML@users.noreply.github.com>
2026-01-06 16:55:38 +0500

    added work

---
+++ Fast_API/CodewithHarryFastapi.py
@@ -0,0 +1,8 @@
+# https://www.youtube.com/watch?v=52c7Kxp_14E
+# This is code with harry video for Fast API Practice first
+
+
+
+
+
+
--- Fast_API/simple_api.py
+++
@@ -1,2 +0,0 @@
-# https://www.youtube.com/watch?v=52c7Kxp_14E
-# This is code with harry video for Fast API Practice first
